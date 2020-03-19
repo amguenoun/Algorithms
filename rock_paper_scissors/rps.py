4b5e976e7e8fdf0f160975806a5aa3a7aa8ae3c9
Ahmad Guenoun <amguenoun@gmail.com>
2020-03-19 17:31:42 -0400

    rps game algo complete

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,27 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    poss = []
+    game = ['rock', 'paper', 'scissors']
+
+    def find_hands(n, game_list):
+        if n == 0:
+            poss.append(game_list)
+            return
+        else:
+            for play in game:
+                find_hands(n-1, game_list + [play])
+
+    find_hands(n, [])
+
+    return poss
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
